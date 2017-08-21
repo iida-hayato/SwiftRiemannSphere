34502453654410bf9da39c722a5ab0ed50f6af3f
iida-hayato <hayato.iida.0213@gmail.com>
2017-08-21 16:24:09 +0900

    swift package init

---
+++ Tests/LinuxMain.swift
@@ -0,0 +1,6 @@
+import XCTest
+@testable import SwiftRiemannSphereTests
+
+XCTMain([
+    testCase(SwiftRiemannSphereTests.allTests),
+])
---
+++ Tests/SwiftRiemannSphereTests/SwiftRiemannSphereTests.swift
@@ -0,0 +1,15 @@
+import XCTest
+@testable import SwiftRiemannSphere
+
+class SwiftRiemannSphereTests: XCTestCase {
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+        XCTAssertEqual(SwiftRiemannSphere().text, "Hello, World!")
+    }
+
+
+    static var allTests = [
+        ("testExample", testExample),
+    ]
+}
